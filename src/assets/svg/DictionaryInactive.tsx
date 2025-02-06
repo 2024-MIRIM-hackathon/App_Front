0609175b12e61576c28b78940f54c4e6b2243555
leesori1410 <w2319@e-mirim.hs.kr>
2025-02-06 23:50:30 +0900

    ✨ feat: 달력 화면 추가(학습 진행 상황 데이터 가져오는 걸로 변경해야 함)

--- src/assets/svg/DictionaryInactive.tsx
+++ src/assets/svg/DictionaryInactive.tsx
@@ -4,8 +4,8 @@ import Svg, { Rect, Path, Mask, SvgProps } from 'react-native-svg';
 const DictionaryInactive: React.FC<SvgProps> = (props) => (
     <Svg width="21" height="22" viewBox="0 0 21 22" fill="none" {...props}>
         <Rect width="21" height="22" fill="white" />
-        <Rect x="2.44995" y="0.75" width="16.1" height="20.5" rx="0.859616" stroke="#5B5B5B" stroke-width="1.5" />
-        <Path d="M12.5098 0.25H15.5298V8.28411L14.424 7.41116C14.187 7.22403 13.8525 7.22403 13.6155 7.41116L12.5098 8.28411V0.25ZM15.6088 8.34651L15.6087 8.34643L15.6088 8.34651ZM12.4307 8.34651C12.4308 8.34649 12.4308 8.34647 12.4308 8.34645L12.4307 8.34651Z" fill="#5B5B5B" stroke="#5B5B5B" stroke-width="0.5" />
+        <Rect x="2.4502" y="0.75" width="16.1" height="20.5" rx="0.859616" stroke="black" stroke-width="1.5" />
+        <Path d="M12.2603 0H15.7803V8.38481C15.7803 8.55291 15.5863 8.64689 15.4544 8.54273L14.2696 7.60738C14.1234 7.49196 13.9171 7.49196 13.7709 7.60738L12.5861 8.54273C12.4542 8.64689 12.2603 8.55291 12.2603 8.38481V0Z" fill="#5B5B5B" />
     </Svg>
 );
 export default DictionaryInactive;
