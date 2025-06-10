b4c5c51def54e10713a755b39faaf0e6e7c0a1e2
leesori1410 <w2319@e-mirim.hs.kr>
2025-06-11 01:45:46 +0900

    🔧 config: axios 전역 설정

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ import {name as appName} from './app.json';
 
 import 'react-native-gesture-handler';
 
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+
 AppRegistry.registerComponent(appName, () => Navigation);
 // AppRegistry.registerComponent(appName, () => App);
